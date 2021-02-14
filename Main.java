7f3fdc83019fa27fe449bb4a6142c83db4af984e
Arongbang <a01053912492@gmail.com>
2021-02-14 08:29:51 +0000

    Finish :)

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
-import java.util.LinkedList;
 
-class Main {
+public class Main {
   public static void main(String[] args) {
+    Graph g = new Graph(9);
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 3);
+    g.addEdge(2, 4);
+    g.addEdge(3, 4);
+    g.addEdge(3, 5);
+    g.addEdge(5, 6);
+    g.addEdge(5, 7);
+    g.addEdge(6, 8);
+
+    g.dfsR(3);
   }
 }
